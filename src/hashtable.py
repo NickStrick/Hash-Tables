21bc836bf3cbccd387f21810020dae761b639686
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-10-01 17:49:27 -0500

    completed djb2  stretch

--- src/hashtable.py
+++ src/hashtable.py
@@ -36,14 +36,21 @@ class HashTable:
 
         OPTIONAL STRETCH: Research and implement DJB2
         '''
-        pass
+
+        hash = 5381
+        for x in key:
+            print(x)
+            # print(1050, 1050 << 1, 1050 << 2,  (hash << 5) + hash, ord(x))
+            hash = ((hash << 5) + hash) + ord(x)
+        # print(hex(hash & 0xFFFFFFFF))
+        return hash & 0xFFFFFFFF
 
     def _hash_mod(self, key):
         '''
         Take an arbitrary key and return a valid integer index
         within the storage capacity of the hash table.
         '''
-        return self._hash(key) % self.capacity
+        return self._hash_djb2(key) % self.capacity
 
     def insert(self, key, value):
         '''
@@ -171,5 +178,7 @@ if __name__ == "__main__":
     print(ht.retrieve("line_1"))
     print(ht.retrieve("line_2"))
     print(ht.retrieve("line_3"))
+
+    print('h', (ht._hash_djb2("line_2")))
 
     print("")
